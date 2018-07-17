dd3782618fade170b4fbef0fba0470996db09b1f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 18:57:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,11 +63,8 @@ class Student
 
   def self.students_below_12th_grade
     sql =  <<-SQL
-    SELECT * FROM students WHERE grade < 12
+    SELECT name FROM students WHERE grade < 12
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
     DB[:conn].execute(sql)
   end
 
