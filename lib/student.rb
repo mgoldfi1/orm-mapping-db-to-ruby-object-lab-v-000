984fa016ad0273fd68f24a6491a0cdec2200db02
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 18:29:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,9 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
