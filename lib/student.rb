cf3976dd8230f31c7d29fa83103395e9366a58c2
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 19:08:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE id = 1
+    SELECT * FROM students WHERE grade = 10
     SQL
     DB[:conn].execute(sql)
   end
