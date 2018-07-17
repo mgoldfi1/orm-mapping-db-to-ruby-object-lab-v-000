7d2729a9305f6c5f73d2115b674c633d39cb5982
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 18:28:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-    SELECT * FROM students WHERE name = ?
+    SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql, name)
   end
